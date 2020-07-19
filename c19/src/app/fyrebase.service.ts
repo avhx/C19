4f3f7d938fb1b95617ee079c7494f3901c4242be
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2020-07-19 05:50:06 -0400

    extra optinal maps

--- c19/src/app/fyrebase.service.ts
+++ c19/src/app/fyrebase.service.ts
@@ -22,4 +22,21 @@ export class FyrebaseService {
       )
     });
   }
+
+  public getCountyDataExtended(countyName:string): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      if(countyName != 'knox') {
+        reject('not knox!');
+      } else {
+        const currDoc = this.firestore.doc('/county/knox/knoxdata/age_data');
+        currDoc.get().subscribe(
+          (val) => {
+            resolve(val.data());
+          }, (err) => {
+            reject(err);
+          }
+        )
+      }
+    });
+  }
 }
