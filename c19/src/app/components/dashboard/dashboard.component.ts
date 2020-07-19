7a5e7fb2a1a845fa35d7103ce1d490ad7bc4e3c1
Ankush Patel <apatel79@vols.utk.edu>
2020-07-18 23:38:43 -0400

    dashboard updates

--- c19/src/app/components/dashboard/dashboard.component.ts
+++ c19/src/app/components/dashboard/dashboard.component.ts
@@ -1,17 +1,94 @@
 import { Component, OnInit } from '@angular/core';
-import { MapService } from 'src/app/map.service';
+import { MapService, LatLng } from 'src/app/map.service';
+import { HttpClient } from '@angular/common/http';
+
+import {} from '@google/maps'; 
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
+
 export class DashboardComponent implements OnInit {
+  map: any;
 
-  constructor(private mapService: MapService) { }
-
-  ngOnInit() {
-
+  constructor(mapService: MapService, private httpClient: HttpClient) {
+    // mapService.loadAPI.then((resolve) => {
+    //   console.log("DONE!");
+    //   var node = document.createElement("google-map");                 // Create a <li> node
+    //   var textnode = document.createTextNode("Water");         // Create a text node
+    //   node.appendChild(textnode);                              // Append the text to <li>
+    //   document.getElementById("myList").appendChild(node);     // Append <li> to <ul> with id="myList" 
+    // }).catch((message) => {
+    //   console.log("Map failed to load!" + message);
+    // })
   }
 
+  ngOnInit() {
+    this.map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 35.7730076, lng: -86.2820081 }, 
+      zoom: 7
+    });
+
+    // var TENNESSEE_BORDER = [
+    //   { lng: -90.36, lat: 34.99 },
+    //   { lng: -90.17, lat: 35.18 },
+    //   { lng: -90.22, lat: 35.44 },
+    //   { lng: -90.00, lat: 35.61 },
+    //   { lng: -89.98, lat: 35.78 },
+    //   { lng: -89.82, lat: 35.84 },
+    //   { lng: -89.67, lat: 36.15 },
+    //   { lng: -89.75, lat: 36.27 },
+    //   { lng: -89.58, lat: 36.39 },
+    //   { lng: -89.55, lat: 36.55 },
+    //   { lng: -88.08, lat: 36.55 },
+    //   { lng: -88.08, lat: 36.73 },
+    //   { lng: -81.60, lat: 36.63 },
+    //   { lng: -81.67, lat: 36.30 },
+    //   { lng: -81.84, lat: 36.29 },
+    //   { lng: -82.00, lat: 36.08 },
+    //   { lng: -82.33, lat: 36.07 },
+    //   { lng: -82.56, lat: 35.90 },
+    //   { lng: -82.68, lat: 35.99 },
+    //   { lng: -82.98, lat: 35.72 },
+    //   { lng: -83.13, lat: 35.72 },
+    //   { lng: -83.48, lat: 35.51 },
+    //   { lng: -83.86, lat: 35.47 },
+    //   { lng: -84.03, lat: 35.22 },
+    //   { lng: -84.24, lat: 35.20 },
+    //   { lng: -84.30, lat: 34.94 },
+    //   { lng: -90.36, lat: 34.99 },
+    // ]
+
+    // var border = new google.maps.Polyline({
+    //   path: TENNESSEE_BORDER,
+    //   geodesic: true,
+    //   strokeColor: "#FF0000",
+    //   strokeOpacity: 1.0,
+    //   strokeWeight: 2
+    // });
+  
+    // border.setMap(this.map);
+
+    this.httpClient.get('./assets/TNCounties.json').subscribe(data => {
+      var COUNTY_INFO = <any[]> data;
+
+      for (var i = 0; i < COUNTY_INFO.length; i++) {
+        // var COUNTY_NAME = data[i][0]["name"];
+        var COUNTY_BORDER = <LatLng[]> data[i][1]["points"];
+
+        var county = new google.maps.Polygon({
+          paths: COUNTY_BORDER,
+          strokeColor: "#FF0000",
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: "#FF0000",
+          fillOpacity: 0.35
+        });
+
+        county.setMap(this.map)
+      }
+    })
+  }
 }
--- c19/src/app/map.service.ts
+++ c19/src/app/map.service.ts
@@ -1,37 +1,39 @@
 import { Injectable } from '@angular/core';
 import { GOOGLE_MAP_JS_API_KEY } from './config';
 
+export interface LatLng {
+  lat: number;
+  lng: number;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
-  loadAPI: Promise<any>
-  isMapLoaded: boolean = false;
+  // loadAPI: Promise<any>
+  // isMapLoaded: boolean = false;
   
   constructor() {
-    console.log("Map Service instantiated!");
-    this.loadAPI = new Promise((resolve) => {
-      this.loadMapAPI();
-      resolve(true);
-    })
-
-    this.loadAPI.then((resolve) => {
-      this.isMapLoaded = resolve;
-      console.log(this.isMapLoaded);
-    }).catch((message) => {
-      console.log("Map failed to load!" + message);
-    })
+    // console.log("Map Service instantiated!");
+    // this.loadAPI = new Promise((resolve) => {
+    //   this.loadMapAPI();
+    //   resolve(true);
+    // })
   }
 
-  public loadMapAPI() {        
-    var script = "https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_MAP_JS_API_KEY;
-    console.log(script);
+  // public loadMapAPI() {        
+  //   var script = "https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_MAP_JS_API_KEY;
+  //   console.log(script);
 
-    let node = document.createElement('script');
-    node.src = script;
-    node.type = 'text/javascript';
-    node.async = false;
-    node.charset = 'utf-8';
-    document.getElementsByTagName('head')[0].appendChild(node);
+  //   let node = document.createElement('script');
+  //   node.src = script;
+  //   node.type = 'text/javascript';
+  //   node.async = false;
+  //   node.charset = 'utf-8';
+  //   document.getElementsByTagName('head')[0].appendChild(node);
+  // }
+
+  public loadMap() {
+    
   }
 }
